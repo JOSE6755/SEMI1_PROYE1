bc90d2123093d35275447ce7bfc367b50e519c39
Jose Martinez <joseamvi58@gmail.com>
2022-09-11 12:33:40 -0600

    Icono de .txt agregado y manejo dinamico de preview de archivo

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react'
 //import Login from "./Components/Login";
-import Register from "./Components/Register";
-/*import Upload from "./Components/Upload_file";
-import Cards from "./Components/Cards";
+//import Register from "./Components/Register";
+import Upload from "./Components/Upload_file";
+/*import Cards from "./Components/Cards";
 import Home from "./Components/Home";*/
 import "./index.css"
 function App() {
   return (
    <div>
-    <Register/>
+    <Upload/>
    </div>
   );
 }
--- Frontend/src/Components/Upload_file.js
+++ Frontend/src/Components/Upload_file.js
@@ -1,20 +1,67 @@
 import React, { useState } from "react";
 import logopdf from "../Images/PDF.png";
+import logotxt from "../Images/TXT.png"
 import "../Styles/Upload_file.css";
+import axios from "axios";
 
 export default function Upload() {
-  const [typeOfFile, setTypeOfFile] = useState("");
+  const [fileInfo, setFileInfo] = useState({});
+  const [preview, setPreview]=useState(null)
+
+  const getFile=(e)=>{
+    const file=e.target.files[0]
+    const prueba=file.name.split(".")
+
+    switch (prueba[1]) {
+      case "pdf":
+        setPreview(logopdf)
+        break
+      
+      case "txt":
+        setPreview(logotxt)
+        break
+    
+      default:
+        const reader=new FileReader()
+        reader.onload=()=>{
+          if(reader.readyState===2){
+            setPreview(reader.result)
+          }
+        }
+        reader.readAsDataURL(file)
+        break
+    }
+  }
+
+
+  const upload=async (e)=>{
+    e.preventDefault();
+
+    const data=new FormData()
+    data.append("file",fileInfo.file)
+    delete fileInfo.file
+    data.append("info",JSON.stringify(fileInfo))
+    console.log(fileInfo)
+
+    try {
+      const result= await axios.post("URL",data)
+      console.log(result)
+    } catch (ex) {
+      console.log(ex)
+    }
+  }
+  
 
   return (
     
-      <form className="formu">
+      <form className="formu" onSubmit={upload}>
         <div className="archivo">
           <label for="name">Nombre archivo</label>
-          <input type={"text"} name="name" className="textos" />
+          <input type={"text"} name="name" className="textos" onChange={(e)=>{setFileInfo({...fileInfo,name:e.target.value})}} />
           
           <span>Archivo seleccionado</span>
           
-          <input type={"file"} name="file" />
+          <input type={"file"} name="file" onChange={getFile} />
    
           <label for="radios">Tipo de archivo</label>
           <div name="radios">
@@ -23,9 +70,9 @@ export default function Upload() {
               type="radio"
               value="public"
               name="public"
-              checked={typeOfFile === "public"}
+              checked={fileInfo.type === "public"}
               onChange={(e) => {
-                setTypeOfFile(e.target.value);
+                setFileInfo({...fileInfo,type:e.target.value});
               }}
             />
             <label for="private">Privado</label>
@@ -33,19 +80,18 @@ export default function Upload() {
               type="radio"
               value="private"
               name="private"
-              checked={typeOfFile === "private"}
+              checked={fileInfo.type === "private"}
               onChange={(e) => {
-                setTypeOfFile(e.target.value);
+                setFileInfo({...fileInfo,type:e.target.value});
               }}
             />
           </div>
           <label for="pass">contrasenia</label>
-          <input type={"password"} className="textos" name="pass"/>
+          <input type={"password"} className="textos" name="pass" onChange={(e)=>setFileInfo({...fileInfo,pass:e.target.value})}/>
         </div>
         <div className="img_submit">
           <div className="img_submit_preview">
-            <img src={logopdf} alt="archivo" />
-            <button type="button">Subir archivo </button>
+            <img src={preview} alt="archivo" />
           </div>
           <div className="img_submit_buttons">
             <button type="reset">Cancelar</button>
