f88ec5cf2a2fcb27136f9249059c680b10251037
Jose Martinez <joseamvi58@gmail.com>
2022-09-12 23:34:51 -0600

    Funcionalidad de login agregada

--- Frontend/src/Components/Login.js
+++ Frontend/src/Components/Login.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { useState } from "react";
 //import { Redirect } from "react-router";
 //import useAuth from "../Auth/UseAuth";
@@ -6,7 +7,7 @@ import "../Styles/Login.css"
 
 export default function Login() {
     
-    const [datos, setDatos] = useState({ userName: null, password: null })
+    const [datos, setDatos] = useState({ user: null, pass: null })
     /*async function probando(e) {
         if (datos.userName != "admin") {
             e.preventDefault()
@@ -25,12 +26,23 @@ export default function Login() {
         }
     }*/
 
+    const verify=async (e)=>{
+        e.preventDefault()
+        const encode=Buffer.from(JSON.stringify(datos)).toString("base64")
+        try {
+            const result=await axios.get(`http://35.209.248.219:3000/api/usuario/login/${encode}`)
+        } catch (ex) {
+            console.log(ex.response.data.message)
+        }
+
+    }
+
     return (
         <div id="CLogin">
-            <form className="Login">
+            <form className="Login" onSubmit={verify}>
                 <h1>Login</h1>
-                <input type="text" placeholder="Username" onChange={(e) => setDatos({ ...datos, userName: e.target.value })} />
-                <input type="password" placeholder="Password" onChange={(e) => setDatos({ ...datos, password: e.target.value })} />
+                <input type="text" placeholder="Username" onChange={(e) => setDatos({ ...datos, user: e.target.value })} />
+                <input type="password" placeholder="Password" onChange={(e) => setDatos({ ...datos, pass: e.target.value })} />
                 <input type="submit" value="Login" />
                 <a href="/" color="white">¿No tienes cuenta?</a>
             </form>
