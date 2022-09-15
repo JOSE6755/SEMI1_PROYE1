0161b056b2a1bc4c1d4737a7c3c062dc3ac16a08
dannloa <3258306811401@ingenieria.usac.edu.gt>
2022-09-15 12:18:34 -0600

    Revert "Cambios"
    This reverts commit ebfc084b4bcb55b089c0f1eaadd8fc1ef92af8ef.

--- Frontend/src/Components/AddFriends.js
+++ Frontend/src/Components/AddFriends.js
@@ -1,117 +1,66 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React from 'react'
 import '../Styles/add.css'
-import useAuth from '../Auth/useAuth';
-
 
 function AddFriends() {
 
-  const { username } = useAuth()
-  const [friends, setFriends] = useState([])
-  const [newFriend, setNewFriend] = useState(null)
-  const [query, setQuery] = useState("")
 
-  const baseUrl = `http://35.209.248.219:3000/api/usuario/getAllUsers/${username()}`
-  const newFriendUrl = 'http://35.209.248.219:3000/api/usuario/addFriend'
+  const numbers = [{
+    name: 'Jason ',
+    age: 74,
+    hasKilled: true,
+    birthday: 'June 13, 1946',
+  },
+  {
+    name: 'Juan',
+    age: 74,
+    hasKilled: true,
+    birthday: 'June 13, 1946',
+  },
+  {
+    name: 'Dani',
+    age: 74,
+    hasKilled: true,
+    birthday: 'June 13, 1946',
+  }]
 
 
-  const listItems = friends.map(({ username, avatar, archivosPublicos }) =>
-    <div>
-      <div class="card-image">
-        <img src={avatar} />
-      </div>
-      <div class="card-text">
-        <h4>Nombre: {username}</h4>
-        <p>Archivos Publicos:{archivosPublicos}</p>
-        <button class="btn-gr" value={[username]} onClick={agregarAmigue} >
-          <svg
-            class="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z"
-            ></path>
-          </svg>
-        </button>
-      </div>
-    </div>
+  const listItems = numbers.map(({ name, age }) =>
+  <div>
+  <div class="card-image">
+  <img src='https://i.imgur.com/wvxPV9S.png'/>
+</div>
+<div class="card-text">
+  <h4>Nombre: {name}</h4>
+  <p>Archivos Publicos:{age}</p>
+  <button class="btn-gr" onClick={agregarAmigue}>
+    <svg
+      class="w-6 h-6"
+      fill="none"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z"
+      ></path>
+    </svg>
+  </button>
+</div>
+</div>
   );
 
 
-  useEffect(() => {
-    getOperations()
-  }, [])
 
-
-
-  const getOperations = async () => {
-    await fetch(baseUrl, {
-      method: 'GET',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-    })
-      .then(resp => resp.json())
-      .then(respuesta => {
-        // console.log("WEEEEEEEEENAAAAAAASSSSSSSS")
-        console.log(respuesta.usuarios)
-        setFriends(respuesta.usuarios)
-        // wenas()
-
-        //setLogs(respuesta)
-      }).catch(console.error)
+  function agregarAmigue(){
+    alert('holaAmigue jsjs')
   }
-
-
-
-  function agregarAmigue(val) {
-    setNewFriend(val.currentTarget.value)
-    console.log(val.currentTarget.value)
-    const addFriendPetition = async () => {
-      const a = {
-        username: username(),
-        newFriend: val.currentTarget.value
-      }
-      console.log("======================SADSAD==========================")
-      console.log(a)
-      await fetch(newFriendUrl, {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          username: username(),
-          newFriend: val.currentTarget.value
-        })
-
-      })
-        .then(resp => resp.json())
-        .then(respuesta => {
-          console.log("WEEEEEEEEENAAAAAAASSSSSSSS")
-          console.log(respuesta)
-          alert(respuesta.message)
-
-
-          //setLogs(respuesta)
-        }).catch(console.error)
-    }
-
-    addFriendPetition()
-  }
-
-
-  console.log(query)
 
   return (
-    <div class="modal-backdrop" >
-      <div class="modal" style={{ height: 600 }} >
+    <div class="modal-backdrop">
+      <div class="modal">
         <header class="modal-header">
           <h2>Agregar Amigos</h2>
           <hr />
@@ -120,22 +69,21 @@ function AddFriends() {
             placeholder="Busca un Usuario"
             type="text"
             v-model="busqueda"
-            onChange={event => setQuery(event.target.value)}
           />
-          <button class="btn-green" id="searchButton"  >
+          <button class="btn-green" id="searchButton">
             Buscar
           </button>
         </header>
 
-        <section class="modal-body" style={{ height: 400 }}>
-          <div class="friends-list" >
+        <section class="modal-body">
+          <div class="friends-list">
             <div
               id="user-info"
               v-for="user of filteredUsers"
               class="card-top"
+              
             >
               {listItems}
-
             </div>
           </div>
         </section>
--- Frontend/src/Components/Login.js
+++ Frontend/src/Components/Login.js
@@ -24,7 +24,6 @@ export default function Login() {
             navegador("/seeFiles",{replace:true})
             navegador("/delete",{replace:true})
             navegador("/edit",{replace:true})
-            navegador("/add",{replace:true})
         } catch (ex) {
             console.log(ex)
         }
